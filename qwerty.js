eec21d4320c6a1e59f54cbf19c2435da9784d592
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-27 17:03:07 +0500

    Create qwerty.js

---
+++ qwerty.js
@@ -0,0 +1,68 @@
+var scriptName = "qwertyCombo";
+var scriptAuthor = "Soulplexis";
+var scriptVersion = 1.0;
+
+var colorIndex = 0;
+
+function randomIntFrom(min,max) // Get a random integer from [min] to [max] 
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function printMessage(message) {
+    var availableColors = ["§c[§4qwertyCombo§c] §7", "§c[§4qwertyCombo§c] §f"];
+
+    var color = availableColors[colorIndex];
+    
+    colorIndex += 1;
+    if (colorIndex >= availableColors.length) {
+        colorIndex = 0;
+    }
+
+    chat.print(color + message + "§r");
+
+}
+var qwertyCombo = moduleManager.getModule("Criticals");
+
+
+function ColorChatModule() {
+    var value = 0;
+
+    this.getName = function() {
+        return "qwertyCombo";
+    }
+
+    this.getCategory = function() {
+        return "Combat";   
+    }
+
+    this.getDescription = function() {
+        return "Counts hits and then criticals.";
+    }
+	this.onEnable = function() {
+		value = 0;
+		criticalstate = Criticals.state;
+	}
+
+    this.onAttack = function() {
+		value += 1;
+        if (mc.thePlayer.motionY < -0.08 || Criticals.state == true) {
+			printMessage("§l§o操你妈!是暴击!! " + value);
+        } else {
+			printMessage("连击! " + value);
+		}
+    }
+	this.onDisable = function() {
+		value = 0;
+	}
+}
+
+var colorChatModule = new ColorChatModule();
+var colorChatModuleClient;
+
+function onEnable() {
+    colorChatModuleClient = moduleManager.registerModule(colorChatModule);
+}
+
+function onDisable() {
+    moduleManager.unregisterModule(colorChatModuleClient);
+}
